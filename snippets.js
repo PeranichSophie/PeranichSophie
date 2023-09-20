2015e381e56e01fac764d29ce55d65268afdddac
Sophie Peranich <98861568+PeranichSophie@users.noreply.github.com>
2023-09-20 14:52:09 +0100

    Add files via upload

---
+++ snippets.js
@@ -0,0 +1,18 @@
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "peranichsophie@gmail.com",
+    Password: "7400692F834FE72EFA54C908EBF66600831B",
+    To: "peranichsophie@gmail.com",
+    From: doccument.getElementById("email").value,
+    Subject: "New Connect from Website Inquiry",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br/> Email: " +
+      document.getElementById("email").value +
+      "<br/> Message: " +
+      document.getElementById("message").value +
+      "<br/>",
+  }).then((message) => alert(message));
+}
